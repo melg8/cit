88c3fb2690b9493b60232fc4bed988c872189141
Melg Eight <public.melg8@gmail.com>
2020-04-02 09:24:11 +0300

    ci(policy): add git log check for typos
    Add check for git log using cspell.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

--- ci/checks/conventional_git_style.sh
+++ ci/checks/conventional_git_style.sh
@@ -5,3 +5,5 @@ conform enforce
 commitlint -t HEAD
 gitlint --commits HEAD
 gitleaks --threads=$(nproc) --verbose --pretty --repo-path=.
+git log --pretty="%h:%B" | \
+cspell -v --config=./ci/checks/dictionaries/cspell.json stdin
