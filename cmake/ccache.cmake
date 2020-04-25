6af8b7f3b89de036ae87bf99769ec0d85ce88324
Melg Eight <public.melg8@gmail.com>
2020-04-25 20:13:57 +0300

    ci: add ccache support
    Add support of ccache usage in build.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

---
+++ cmake/ccache.cmake
@@ -0,0 +1,9 @@
+option(USE_CCACHE "Enable use of ccache")
+
+if(${USE_CCACHE})
+  find_program(CCACHE_FOUND ccache)
+  if(CCACHE_FOUND)
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
+  endif()
+endif()
