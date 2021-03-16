e160d2926e5fd1f6dee0efe7971dd7b13d5adf93
Melg Eight <public.melg8@gmail.com>
2021-03-16 22:03:40 +0300

    ci(docker): remove git-cop temporary
    Remove git-cop that is not installed now, it
    will be replaced with git-lint.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

--- ci/checks/conventional_git_style.sh
+++ ci/checks/conventional_git_style.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 set -e
+
 command time -v conform enforce
 command time -v commitlint -t HEAD
 command time -v gitlint --commits HEAD
 command time -v  gitleaks --threads="$(nproc)" --verbose --pretty --repo-path=.
 command time -v  git log --pretty="%h:%B" | \
-cspell -v --config=./ci/checks/dictionaries/cspell.json stdin \
-command time -v git-cop -p
+cspell -v --config=./ci/checks/dictionaries/cspell.json stdin
