e6f182ba4a8bb6df9253c907c646a332979be4d3
Melg Eight <public.melg8@gmail.com>
2021-03-28 22:51:45 +0300

    chore(nix): add gitleaks support
    Add nix expression to load and use gitleaks.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

---
+++ ci/nix/git_leaks.nix
@@ -0,0 +1,21 @@
+{ lib, buildGoPackage, fetchFromGitHub }:
+
+buildGoPackage rec {
+  pname = "gitleaks";
+  version = "7.3.0";
+
+  goPackagePath = "github.com/zricethezav/gitleaks";
+  goDeps = ./git_leaks_deps.nix;
+
+  src = fetchFromGitHub {
+    owner = "zricethezav";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "0mxx3jmm2k9w6yjzsxnnp6k2w42k24zvnfcybppil8kfh6cax5i0";
+  };
+
+  meta = with lib; {
+    description = "Policy enforcement for your pipelines.";
+    license = licenses.mit;
+  };
+}
