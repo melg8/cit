2f3fd5f8383e0a30c6535126f8dbc4c1a9628efc
Melg Eight <public.melg8@gmail.com>
2021-03-30 22:55:10 +0300

    chore(nix): add nixpkgs-fmt to nix
    Add nix formatting tool to docker image.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

--- ci/nix/docker_build.nix
+++ ci/nix/docker_build.nix
@@ -17,9 +17,9 @@ in
 rec {
   world = pkgs.dockerTools.buildLayeredImage {
     name = "world";
-    tag = "0.0.1";
+    tag = "0.0.3";
     contents = [
-      # All together 817 MB
+      # All together 839 MB
       pvs_studio_for_free
       pkgs.git
 
@@ -31,8 +31,7 @@ rec {
 
       # gem
       # Together 316 MB  (295 on ruby 2.7)
-      mdl # ruby 2.7 261.4 MB
-      #pkgs.mdl # 260.4 MB
+      #mdl # ruby 2.7 261.4 MB
       git_lint # 294 MB
 
       # npm
@@ -49,6 +48,11 @@ rec {
       pkgs.yamllint # 140 MB
       pkgs.cmake-format # 150 MB
       git_lint_py # 139 MB
+
+      # Potentially later.
+      # Together 77 MB (+ 22 MB to total image size).
+      pkgs.nix
+      pkgs.nixpkgs-fmt
     ];
   };
 }
