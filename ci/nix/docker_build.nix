628615f61958331f3aaa26a78cb3a21a8b963784
Melg Eight <public.melg8@gmail.com>
2021-03-30 22:06:05 +0300

    chore(nix): add ls lint to nix
    Add ls-lint as nix expression.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

--- ci/nix/docker_build.nix
+++ ci/nix/docker_build.nix
@@ -5,6 +5,7 @@ let
   git_leaks = pkgs.callPackage ./git_leaks/default.nix { };
   git_lint = pkgs.callPackage ./git_lint/default.nix { };
   git_lint_py = pkgs.callPackage ./git_lint_py/default.nix { };
+  ls_lint = pkgs.callPackage ./ls_lint/default.nix { };
   mdl = pkgs.mdl.override (old: {
     bundlerApp = attrs: old.bundlerApp (attrs // {
       ruby = pkgs.ruby_2_7;
@@ -16,9 +17,9 @@ in
 rec {
   world = pkgs.dockerTools.buildLayeredImage {
     name = "world";
-    tag = "0.1.1";
+    tag = "0.0.1";
     contents = [
-      # All together 815 MB
+      # All together 817 MB
       pvs_studio_for_free
       pkgs.git
 
@@ -26,6 +27,7 @@ rec {
       pkgs.git-sizer # 37 MB
       conform # 57 MB
       git_leaks # 44 MB
+      ls_lint # 4.2 MB
 
       # gem
       # Together 316 MB  (295 on ruby 2.7)
--- ci/nix/ls_lint/default.nix
+++ ci/nix/ls_lint/default.nix
@@ -4,14 +4,14 @@ buildGoPackage rec {
   pname = "ls-lint";
   version = "1.9.2";
 
-  goPackagePath = "github.com/talos-systems/conform";
-  #goDeps = ./deps.nix;
+  goPackagePath = "github.com/loeffel-io/ls-lint/";
+  goDeps = ./deps.nix;
 
   src = fetchFromGitHub {
     owner = "loeffel-io";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0c2nzlbdirsa7w5zs06vlqsc6z2yi5ic3dzwad8shizxa4pv000";
+    sha256 = "1wykmsycbkssvclqk4lfmdsmh83hzq34klbn0pdwr563vwh0p3v3";
   };
 
   meta = with lib; {
