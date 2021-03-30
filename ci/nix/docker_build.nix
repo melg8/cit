02f48d5ecaf4981cdb0911e0361da34fb4471ea1
Melg Eight <public.melg8@gmail.com>
2021-03-30 16:16:15 +0300

    chore(nix): add more tools to contents of image
    Add more tools to docker image.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

--- ci/nix/docker_build.nix
+++ ci/nix/docker_build.nix
@@ -4,37 +4,45 @@ let
     conform = pkgs.callPackage ./conform.nix {};
     git_leaks = pkgs.callPackage ./git_leaks.nix {};
     git_lint = pkgs.callPackage ./git_lint/default.nix {};
+    mdl = pkgs.mdl.override (old: {
+        bundlerApp = attrs: old.bundlerApp (attrs // {
+         ruby = pkgs.ruby_2_7;
+
+         });
+        }
+    );
 in rec {
  world = pkgs.dockerTools.buildLayeredImage {
     name = "world";
-    tag = "0.0.15";
+    tag = "0.1.0";
     contents = [
-                 # pvs_studio_for_free
+                 pvs_studio_for_free
                  pkgs.git
 
                  # go
-                 # pkgs.git-sizer # 37 MB
-                 # conform # 57 MB
-                 # git_leaks 44 MB
+                 pkgs.git-sizer # 37 MB
+                 conform # 57 MB
+                 git_leaks # 44 MB
 
                  # gem
-                 # Together 316 MB
-                 # pkgs.mdl # 260 MB
+                 # Together 316 MB  (295 on ruby 2.7)
+                 mdl # ruby 2.7 261.4 MB
+                 #pkgs.mdl # 260.4 MB
                  git_lint # 294 MB
 
                  # npm
-                 # pkgs.nodePackages_latest.textlint # 259 MB
+                 pkgs.nodePackages_latest.textlint # 259 MB
 
                  # Haskell
                  # Together 52 MB
-                 # pkgs.hadolint # 47 MB
-                 # pkgs.shellcheck # 45 MB
+                 pkgs.hadolint # 47 MB
+                 pkgs.shellcheck # 45 MB
 
                  # python 3
                  # Together 152 MB
-                 # pkgs.codespell # 140 MB
-                 # pkgs.yamllint # 140 MB
-                 # pkgs.cmake-format # 150 MB
+                 pkgs.codespell # 140 MB
+                 pkgs.yamllint # 140 MB
+                 pkgs.cmake-format # 150 MB
                  ];
 };
 }
