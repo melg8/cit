7edffbab3c6ce06b2a5721b2138aae94e5040baa
Melg Eight <public.melg8@gmail.com>
2021-04-01 20:28:36 +0300

    ci(policy): add files to skip in spell check
    Add files that should be skipped by spell check.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

--- ci/checks/spelling.sh
+++ ci/checks/spelling.sh
@@ -3,7 +3,9 @@
 DICTIONARY="./ci/checks/dictionaries/spelling_corrections.txt"
 SKIP=".git,\
 ./ci/checks/dictionaries/*,\
-./ci/docker/docker_files/scripts/cleanups/permissions.sh"
+./ci/docker/docker_files/scripts/cleanups/permissions.sh,\
+*package-lock.json,\
+*node-packages.nix"
 
 set -e
 cspell -v --config=./ci/checks/dictionaries/cspell.json "**/*.*"
