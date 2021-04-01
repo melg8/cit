676b3e5a67fb15f1401cb6ffda2e3a8ac2941f2f
Melg Eight <public.melg8@gmail.com>
2021-04-01 23:08:06 +0300

    ci(docker): fix git leaks check
    Update gitleaks to version 7 and fix check.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

--- ci/checks/conventional_git_style.sh
+++ ci/checks/conventional_git_style.sh
@@ -5,7 +5,7 @@ set -e
 command time -v conform enforce
 command time -v commitlint -t HEAD
 command time -v gitlint --commits HEAD
-command time -v  gitleaks --threads="$(nproc)" --verbose --pretty --repo-path=.
+command time -v  gitleaks --verbose path=.
 command time -v  git log --pretty="%h:%B" | \
 cspell -v --config=./ci/checks/dictionaries/cspell.json stdin
 command time -v git-lint -a
