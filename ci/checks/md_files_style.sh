0d1edba023e2da9b6470da11ea098608e9406bac
Melg Eight <public.melg8@gmail.com>
2021-04-11 15:46:01 +0300

    ci(policy): fix md docs policy
    Disable one of rules to be consistent with other
    linters.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

--- ci/checks/md_files_style.sh
+++ ci/checks/md_files_style.sh
@@ -2,16 +2,11 @@
 
 set -e
 
-MODULES_PATH=/usr/lib/node_modules
-
-remark . -f \
---use ${MODULES_PATH}/remark-preset-lint-consistent \
---use ${MODULES_PATH}/remark-preset-lint-markdown-style-guide \
---use ${MODULES_PATH}/remark-preset-lint-recommended
+remark . -r rc.yaml -f
 
 mdl .
 
 ignore=license.md
 
 find "$(pwd)" -name "*.md" ! -iname "${ignore}" \
--exec textlint {} +
+-exec textlint --experimental --parallel {} +
