cde70636ef1134b900dbaa9e333b416738457370
Melg Eight <public.melg8@gmail.com>
2021-04-23 18:28:41 +0300

    chore(nix): add build from docker
    Add build of docker image from inside of
    current docker image with nix.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

---
+++ ci/docker/build_with_nix.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+nix build -f ./ci/nix/docker_build.nix \
+             --enforce-determinism \
+             --keep-failed
+ls -la .
+sha256sum ./result
---
+++ ci/docker/docker_run_with_network.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+set -e
+
+COMMAND=$*
+echo "command with args: " "${COMMAND}"
+
+docker container run --rm --ulimit nofile=1024 \
+-v "$(pwd)":/home/user/work \
+melg8/cit:0.0.6 "${COMMAND}"
