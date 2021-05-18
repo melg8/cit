c38bb7b5e63898ddf7120d9beb9467e1bcb94266
Melg Eight <public.melg8@gmail.com>
2021-05-18 23:20:04 +0300

    refactor(nix): replace raw text with generator
    Replace raw kaem script text with generated through
    nix based functions.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

--- bootstrap_nix/bootstrap_seeds/generate_kaem_scripts.nix
+++ bootstrap_nix/bootstrap_seeds/generate_kaem_scripts.nix
@@ -1,15 +1,42 @@
 { sources }:
 let
   kaem-addition = ./kaem.c;
+  prepare-sources = list: builtins.concatStringsSep " "
+    (builtins.map (x: "-f " + x) list) + " ";
+
+  build-with-m2 = with sources; { builder, elf, sources, name }: ''
+    ${builder} --debug --architecture x86 ''
+  + prepare-sources sources +
+  ''-o ${name}.M1
+
+    ${elf} -f ${name}.M1 -o ${name}-footer.M1
+
+    ./bin_M1 -f ${stage0-posix}/x86/x86_defs.M1 \
+      -f ${stage0-posix}/x86/libc-core.M1 \
+      -f ${name}.M1 \
+      -f ${name}-footer.M1 \
+      --LittleEndian \
+      --architecture x86 \
+      -o ${name}.hex2
+
+    ./bin_hex2 -f ${stage0-posix}/x86/ELF-i386-debug.hex2 \
+      -f ${name}.hex2 \
+      --LittleEndian \
+      --architecture x86 \
+      --BaseAddress 0x8048000 \
+      -o ./bin_${name} \
+      --exec_enable
+  '';
+
 in
 with sources;
 rec {
-  kaem_run = builtins.toFile "kaem.run" ''
+  kaem_run = builtins.toFile "kaem.run" (''
     ${bootstrap-seeds}/POSIX/x86/hex0-seed ${bootstrap-seeds}/POSIX/x86/hex0_x86.hex0 hex0
     ./hex0 ${bootstrap-seeds}/POSIX/x86/kaem-minimal.hex0 kaem-0
 
     ./hex0 ${stage0-posix}/x86/hex1_x86.hex0 hex1
-    ./hex0 ${stage0-posix}/x86/catm_x86.hex0 catm ## why hex0? maybe hex1 already?
+    ./hex0 ${stage0-posix}/x86/catm_x86.hex0 catm
     ./hex1 ${stage0-posix}/x86/hex2_x86.hex1 hex2-0
 
     ./catm hold ${stage0-posix}/x86/ELF-i386.hex2 ${stage0-posix}/x86/M0_x86.hex2
@@ -157,163 +184,105 @@ rec {
       --BaseAddress 0x8048000 \
       -o bin_kaem \
       --exec_enable
-
+  '' +
+  build-with-m2
+    {
+      name = "blood-elf";
+      builder = "./M2";
+      elf = "./blood-elf-0";
+      sources = [
+        "${m2-planet}/test/common_x86/functions/exit.c"
+        "${m2-planet}/test/common_x86/functions/file.c"
+        "${m2-planet}/functions/file_print.c"
+        "${mescc-tools}/functions/numerate_number.c"
+        "${m2-planet}/test/common_x86/functions/malloc.c"
+        "${m2-planet}/functions/calloc.c"
+        "${m2-planet}/functions/match.c"
+        "${m2-planet}/functions/require.c"
+        "${m2-planet}/functions/in_set.c"
+        "${mescc-tools}/blood-elf.c"
+      ];
+    }
+  +
+  build-with-m2 {
+    name = "get_machine";
+    builder = "./M2";
+    elf = "./bin_blood-elf";
+    sources = [
+      "${m2-planet}/test/common_x86/functions/exit.c"
+      "${m2-planet}/test/common_x86/functions/file.c"
+      "${m2-planet}/functions/file_print.c"
+      "${m2-planet}/test/common_x86/functions/malloc.c"
+      "${m2-planet}/functions/calloc.c"
+      "${m2-planet}/test/common_x86/functions/uname.c"
+      "${m2-planet}/functions/match.c"
+      "${mescc-tools}/get_machine.c"
+    ];
+  }
+  +
+  build-with-m2 {
+    name = "M2-Planet";
+    builder = "./M2";
+    elf = "./bin_blood-elf";
+    sources = [
+      "${m2-planet}/test/common_x86/functions/file.c"
+      "${m2-planet}/test/common_x86/functions/malloc.c"
+      "${m2-planet}/functions/calloc.c"
+      "${m2-planet}/test/common_x86/functions/exit.c"
+      "${m2-planet}/functions/match.c"
+      "${m2-planet}/functions/in_set.c"
+      "${m2-planet}/functions/numerate_number.c"
+      "${m2-planet}/functions/file_print.c"
+      "${m2-planet}/functions/number_pack.c"
+      "${m2-planet}/functions/string.c"
+      "${m2-planet}/functions/require.c"
+      "${m2-planet}/functions/fixup.c"
+      "${m2-planet}/cc.h"
+      "${m2-planet}/cc_globals.c"
+      "${m2-planet}/cc_reader.c"
+      "${m2-planet}/cc_strings.c"
+      "${m2-planet}/cc_types.c"
+      "${m2-planet}/cc_core.c"
+      "${m2-planet}/cc.c"
+    ];
+  }
+  +
+  build-with-m2 {
+    name = "mes-m2";
+    builder = "./bin_M2-Planet";
+    elf = "./bin_blood-elf";
+    sources = [
+      "${mes-m2}/mes.h"
+      "${m2-planet}/test/common_x86/functions/file.c"
+      "${m2-planet}/test/common_x86/functions/exit.c"
+      "${m2-planet}/test/common_x86/functions/malloc.c"
+      "${m2-planet}/functions/calloc.c"
+      "${mes-m2}/mes.c"
+      "${mes-m2}/mes_cell.c"
+      "${mes-m2}/mes_builtins.c"
+      "${mes-m2}/mes_eval.c"
+      "${mes-m2}/mes_print.c"
+      "${mes-m2}/mes_read.c"
+      "${mes-m2}/mes_tokenize.c"
+      "${mes-m2}/mes_vector.c"
+      "${mes-m2}/mes_list.c"
+      "${mes-m2}/mes_string.c"
+      "${mes-m2}/mes_keyword.c"
+      "${mes-m2}/mes_record.c"
+      "${mes-m2}/mes_init.c"
+      "${mes-m2}/mes_macro.c"
+      "${mes-m2}/mes_posix.c"
+      "${mes-m2}/functions/numerate_number.c"
+      "${mes-m2}/functions/match.c"
+      "${mes-m2}/functions/in_set.c"
+      "${mes-m2}/functions/file_print.c"
+    ];
+  }
+  + ''
     ./bin_kaem --verbose --strict -f ${kaem_full_run}
-  '';
+  '');
   kaem_full_run = builtins.toFile "kaem1.run" ''
     cd /build
-
-    # Build blood-elf from C sources.
-    ./M2 --architecture x86 \
-      -f ${m2-planet}/test/common_x86/functions/exit.c \
-      -f ${m2-planet}/test/common_x86/functions/file.c \
-      -f ${m2-planet}/functions/file_print.c \
-      -f ${mescc-tools}/functions/numerate_number.c \
-      -f ${m2-planet}/test/common_x86/functions/malloc.c \
-      -f ${m2-planet}/functions/calloc.c \
-      -f ${m2-planet}/functions/match.c \
-      -f ${m2-planet}/functions/require.c \
-      -f ${m2-planet}/functions/in_set.c \
-      -f ${mescc-tools}/blood-elf.c \
-      --debug \
-      -o blood-elf.M1
-
-    ./blood-elf-0 -f blood-elf.M1 -o blood-elf-footer.M1
-
-    ./bin_M1 -f ${stage0-posix}/x86/x86_defs.M1 \
-      -f ${stage0-posix}/x86/libc-core.M1 \
-      -f blood-elf.M1 \
-      -f blood-elf-footer.M1 \
-      --LittleEndian \
-      --architecture x86 \
-      -o hold
-
-    ./bin_hex2 -f ${stage0-posix}/x86/ELF-i386-debug.hex2 \
-      -f hold \
-      --LittleEndian \
-      --architecture x86 \
-      --BaseAddress 0x8048000 \
-      -o ./bin_blood-elf \
-      --exec_enable
-
-    # Build get_machine from C sources.
-    ./M2 --architecture x86 \
-      -f ${m2-planet}/test/common_x86/functions/exit.c \
-      -f ${m2-planet}/test/common_x86/functions/file.c \
-      -f ${m2-planet}/functions/file_print.c \
-      -f ${m2-planet}/test/common_x86/functions/malloc.c \
-      -f ${m2-planet}/functions/calloc.c \
-      -f ${m2-planet}/test/common_x86/functions/uname.c \
-      -f ${m2-planet}/functions/match.c \
-      -f ${mescc-tools}/get_machine.c \
-      --debug \
-      -o get_machine.M1
-    
-    ./bin_blood-elf -f get_machine.M1 -o get_machine-footer.M1
-    
-    ./bin_M1 -f ${stage0-posix}/x86/x86_defs.M1 \
-      -f ${stage0-posix}/x86/libc-core.M1 \
-      -f get_machine.M1 \
-      -f get_machine-footer.M1 \
-      --LittleEndian \
-      --architecture x86 \
-      -o hold
-    
-    ./bin_hex2 -f ${stage0-posix}/x86/ELF-i386-debug.hex2 \
-      -f hold \
-      --LittleEndian \
-      --architecture x86 \
-      --BaseAddress 0x8048000 \
-      -o ./bin_get_machine \
-      --exec_enable
-
-    # Build M2-planet from M2-planet.
-    ./M2 --architecture x86 \
-      -f ${m2-planet}/test/common_x86/functions/file.c \
-      -f ${m2-planet}/test/common_x86/functions/malloc.c \
-      -f ${m2-planet}/functions/calloc.c \
-      -f ${m2-planet}/test/common_x86/functions/exit.c \
-      -f ${m2-planet}/functions/match.c \
-      -f ${m2-planet}/functions/in_set.c \
-      -f ${m2-planet}/functions/numerate_number.c \
-      -f ${m2-planet}/functions/file_print.c \
-      -f ${m2-planet}/functions/number_pack.c \
-      -f ${m2-planet}/functions/string.c \
-      -f ${m2-planet}/functions/require.c \
-      -f ${m2-planet}/functions/fixup.c \
-      -f ${m2-planet}/cc.h \
-      -f ${m2-planet}/cc_globals.c \
-      -f ${m2-planet}/cc_reader.c \
-      -f ${m2-planet}/cc_strings.c \
-      -f ${m2-planet}/cc_types.c \
-      -f ${m2-planet}/cc_core.c \
-      -f ${m2-planet}/cc.c \
-      --debug \
-      -o M2.M1
-    
-    ./bin_blood-elf -f M2.M1 -o M2-footer.M1
-    
-    ./bin_M1 -f ${stage0-posix}/x86/x86_defs.M1 \
-      -f ${stage0-posix}/x86/libc-core.M1 \
-      -f M2.M1 \
-      -f M2-footer.M1 \
-      --LittleEndian \
-      --architecture x86 \
-      -o hold
-    
-    ./bin_hex2 -f ${stage0-posix}/x86/ELF-i386-debug.hex2 \
-      -f hold \
-      --LittleEndian \
-      --architecture x86 \
-      --BaseAddress 0x8048000 \
-      -o ./bin_M2-Planet \
-      --exec_enable
-
-    # Build Mes-m2 from M2-Planet.
-    ./bin_M2-Planet --debug --architecture x86 \
-      -f ${mes-m2}/mes.h \
-      -f ${m2-planet}/test/common_x86/functions/file.c \
-      -f ${m2-planet}/test/common_x86/functions/exit.c \
-      -f ${m2-planet}/test/common_x86/functions/malloc.c \
-      -f ${m2-planet}/functions/calloc.c \
-      -f ${mes-m2}/mes.c \
-      -f ${mes-m2}/mes_cell.c \
-      -f ${mes-m2}/mes_builtins.c \
-      -f ${mes-m2}/mes_eval.c \
-      -f ${mes-m2}/mes_print.c \
-      -f ${mes-m2}/mes_read.c \
-      -f ${mes-m2}/mes_tokenize.c \
-      -f ${mes-m2}/mes_vector.c \
-      -f ${mes-m2}/mes_list.c \
-      -f ${mes-m2}/mes_string.c \
-      -f ${mes-m2}/mes_keyword.c \
-      -f ${mes-m2}/mes_record.c \
-      -f ${mes-m2}/mes_init.c \
-      -f ${mes-m2}/mes_macro.c \
-      -f ${mes-m2}/mes_posix.c \
-      -f ${mes-m2}/functions/numerate_number.c \
-      -f ${mes-m2}/functions/match.c \
-      -f ${mes-m2}/functions/in_set.c \
-      -f ${mes-m2}/functions/file_print.c \
-      -o mes.M1
-
-    ./bin_blood-elf -f mes.M1 -o mes-footer.M1
-
-    ./bin_M1 -f ${m2-planet}/test/common_x86/x86_defs.M1 \
-      -f ${m2-planet}/test/common_x86/libc-core.M1 \
-      -f mes.M1 \
-      -f mes-footer.M1 \
-      --LittleEndian \
-      --architecture x86 \
-      -o mes.hex2
-
-    ./bin_hex2 -f ${m2-planet}/test/common_x86/ELF-i386-debug.hex2 \
-      -f mes.hex2 \
-      --LittleEndian \
-      --architecture x86 \
-      --BaseAddress 0x8048000 \
-      -o ./bin_mes-m2 \
-      --exec_enable
 
     mkdir ''${out}
     ./catm ''${out}/blood-elf-0 ./blood-elf-0
@@ -321,7 +290,6 @@ rec {
 
     ./catm ''${out}/M2 ./M2
     chmod_x ''${out}/M2
-
 
     mkdir ''${out}/bin
     ./catm ''${out}/bin/new_kaem ./bin_kaem
@@ -347,7 +315,5 @@ rec {
 
     ./catm ''${out}/bin/mes-m2 ./bin_mes-m2
     chmod_x ''${out}/bin/mes-m2
-
-
   '';
 }
--- bootstrap_nix/bootstrap_seeds/sources.nix
+++ bootstrap_nix/bootstrap_seeds/sources.nix
@@ -11,18 +11,23 @@
     ref = "refs/heads/master";
     rev = "358b6cfb96e1685891a705a0bb31eda499d57974"; # Release 1.7.0
   };
-  m2-libc =
-    builtins.fetchGit rec {
-      name = "M2libc";
-      url = "https://github.com/oriansj/${name}.git";
-      ref = "refs/heads/main";
-      rev = "dd470ed9aea3ed00ce2924daaabe49311975903a"; # Release 1.7.0
-    };
+  m2-libc = builtins.fetchGit rec {
+    name = "M2libc";
+    url = "https://github.com/oriansj/${name}.git";
+    ref = "refs/heads/main";
+    rev = "dd470ed9aea3ed00ce2924daaabe49311975903a"; # Release 1.7.0
+  };
   mescc-tools = builtins.fetchGit rec {
     name = "mescc-tools";
     url = "https://github.com/oriansj/${name}.git";
     ref = "refs/heads/master";
     rev = "3f5b7588a20bc2225f25a31eed53a8152563955f"; # Use patched version with m2-planet 1.7.0
+  };
+  mescc-tools-extra = builtins.fetchGit rec {
+    name = "mescc-tools-extra";
+    url = "https://github.com/oriansj/${name}.git";
+    ref = "refs/heads/master";
+    rev = "5eadec73e8ea309a7a0a8e8409315c49209c1fb4";
   };
   stage0-posix = builtins.fetchGit rec {
     name = "stage0-posix";
