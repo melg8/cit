3fa791a7047880141c06d060dd7897f8e756f96f
Melg Eight <public.melg8@gmail.com>
2022-03-28 19:40:54 +0300

    ci: update conform
    Update conform linter.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

--- ci/nix/conform/default.nix
+++ ci/nix/conform/default.nix
@@ -1,18 +1,19 @@
-{ lib, buildGoPackage, fetchFromGitHub }:
+{ lib, buildGoModule, fetchFromGitHub, git }:
 
-buildGoPackage rec {
+buildGoModule rec {
   pname = "conform";
-  version = "0.1.0-alpha.20";
-
-  goPackagePath = "github.com/talos-systems/conform";
-  goDeps = ./deps.nix;
+  version = "0.1.0-alpha.25";
 
   src = fetchFromGitHub {
     owner = "talos-systems";
     repo = pname;
     rev = "refs/tags/v${version}";
-    sha256 = "0c2nzlbdirsa7w5zs06vlqsc6z2yi5ic3dzwad8shizxa4pv3q2m";
+    sha256 = "0v11z6xr16lp4wb68g4nn318arfkray8sjwbcc0q2gka1a5a01as";
   };
+
+  nativeBuildInputs = [ git ];
+
+  vendorSha256 = "18ggj4wskbprwmky5smfspabba0y9c3xginp890kiqhas3p2sa1s";
 
   meta = with lib; {
     description = "Policy enforcement for your pipelines.";
