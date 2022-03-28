c1a3b7a97aeefda78646e1ff783eccb006c4fdd7
Melg Eight <public.melg8@gmail.com>
2022-03-28 20:36:11 +0300

    ci: update ls_lint
    Update ls_lint linter.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

--- ci/nix/ls_lint/default.nix
+++ ci/nix/ls_lint/default.nix
@@ -1,18 +1,17 @@
-{ lib, buildGoPackage, fetchFromGitHub }:
+{ lib, buildGoModule, fetchFromGitHub}:
 
-buildGoPackage rec {
+buildGoModule rec {
   pname = "ls-lint";
-  version = "1.9.2";
-
-  goPackagePath = "github.com/loeffel-io/ls-lint/";
-  goDeps = ./deps.nix;
+  version = "1.11.0";
 
   src = fetchFromGitHub {
     owner = "loeffel-io";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1wykmsycbkssvclqk4lfmdsmh83hzq34klbn0pdwr563vwh0p3v3";
+    sha256 = "1a9i4wdqvqaz8a0wd1yz7p8f8jdgrwm23f7pq3kflwg1s7086bpn";
   };
+
+  vendorSha256 = "1wnwr6nzgpr6yr9v269ybmq0nfhfv681l7bkl1ilzb6p6yvjj1kz";
 
   meta = with lib; {
     description = "An extremely fast directory and filename linter - Bring some structure to your project directories";
