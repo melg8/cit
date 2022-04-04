b27139b35d9d4396ac59cc4a96b7ad0b03f31f4d
Melg Eight <public.melg8@gmail.com>
2022-04-04 19:16:53 +0300

    build(policy): add compiler flags
    Add compiler flags for c++14 standard, sanitizers and
    compile-time checks.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

--- sources/example_glassy/library/sources/bignum.h
+++ sources/example_glassy/library/sources/bignum.h
@@ -1,7 +1,30 @@
 #ifndef BIGNUM_H
 #define BIGNUM_H
 
+#include <openssl/bn.h>
+#include <outcome.hpp>
+
 namespace glassy {
+
+using BnUlong = BN_ULONG;
+namespace outcome = OUTCOME_V2_NAMESPACE;
+
+template <typename T>
+using Result = outcome::result<T>;
+
+class BigNum {
+ public:
+  static Result<BigNum> fromBnUlong(BnUlong value) noexcept;
+
+ private:
+  int v;
+};
+
+inline Result<BigNum> BigNum::fromBnUlong(BnUlong value) noexcept {
+  BigNum v;
+  v.v = value;
+  return v;
+}
 
 inline int Sum(int a, int b) { return a + b; }
 
--- sources/example_glassy/tests/sources/bignum_test.cpp
+++ sources/example_glassy/tests/sources/bignum_test.cpp
@@ -3,6 +3,8 @@
 #include <bignum.h>
 
 SCENARIO("bignum") {
+  using namespace glassy;
+
   GIVEN("two values") {
     const auto value_1 = 1;
     const auto value_2 = 2;
@@ -13,6 +15,22 @@ SCENARIO("bignum") {
       THEN("result is equal to expected") {
         const auto expected = 3;
         CHECK_EQ(result, expected);
+      }
+    }
+  }
+
+  GIVEN("any BnUlong value") {
+    const BnUlong value = 1;
+
+    WHEN("creating Bignum from it") {
+      const auto result = BigNum::fromBnUlong(value);
+
+      THEN("result is not empty") { CHECK(result); }
+
+      THEN("result converted back to BnUlong equal to original value") {
+        //        const auto converted_back = BigNum::ToUlong(*result);
+
+        //        CHECK_EQ(converted_back, value);
       }
     }
   }
