2d650de662e6b6854ee8f0d33f57cac382547c4b
Melg Eight <public.melg8@gmail.com>
2022-05-05 11:01:31 +0300

    chore: reduce fields
    Reduce fields.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

--- sources/example_glassy/application/sources/main.cpp
+++ sources/example_glassy/application/sources/main.cpp
@@ -3,8 +3,6 @@
 #include <memory>
 #include <type_traits>
 
-namespace etl {
-
 class imemory_block_allocator {
  public:
   imemory_block_allocator() {}
@@ -12,21 +10,12 @@ class imemory_block_allocator {
   virtual ~imemory_block_allocator() {}
 
  private:
-  imemory_block_allocator(const etl::imemory_block_allocator&) = delete;
+  imemory_block_allocator(const imemory_block_allocator&) = delete;
   imemory_block_allocator& operator=(const imemory_block_allocator&) = delete;
 };
-}  // namespace etl
-
-namespace etl {
 
 template <size_t VBlock_Size, size_t VAlignment, size_t VSize>
-class fixed_sized_memory_block_allocator : public imemory_block_allocator {
- public:
-  fixed_sized_memory_block_allocator() {}
-};
-}  // namespace etl
-
-namespace etl {
+struct fixed_sized_memory_block_allocator : public imemory_block_allocator {};
 
 typedef uint_least8_t message_id_t;
 
@@ -35,30 +24,21 @@ class imessage {
   virtual ~imessage() {}
 };
 
-template <etl::message_id_t ID_, typename TParent = etl::imessage>
+template <message_id_t ID_, typename TParent = imessage>
 class message : public TParent {
  public:
   enum { ID = ID_ };
 };
-}  // namespace etl
 
-namespace etl {
 class ireference_counted_message;
 
 class ireference_counted_message_pool {
  public:
   virtual ~ireference_counted_message_pool() {}
 };
-}  // namespace etl
-
-namespace etl {
 
 typedef std::atomic<int> atomic_int;
 typedef std::atomic<int32_t> atomic_int32_t;
-
-}  // namespace etl
-
-namespace etl {
 
 class ireference_counter {
  public:
@@ -86,7 +66,7 @@ class ireference_counted_object {
 };
 
 template <typename TObject, typename TCounter>
-class reference_counted_object : public etl::ireference_counted_object {
+class reference_counted_object : public ireference_counted_object {
  public:
   typedef TObject value_type;
   typedef TCounter counter_type;
@@ -98,12 +78,8 @@ class reference_counted_object : public etl::ireference_counted_object {
   reference_counted_object& operator=(const reference_counted_object&) = delete;
 
   TObject object;
-  etl::reference_counter<TCounter> reference_counter;
+  reference_counter<TCounter> reference_counter_;
 };
-
-}  // namespace etl
-
-namespace etl {
 
 class ireference_counted_message {
  public:
@@ -111,47 +87,42 @@ class ireference_counted_message {
 };
 
 template <typename TMessage, typename TCounter>
-class reference_counted_message : public etl::ireference_counted_message {
+class reference_counted_message : public ireference_counted_message {
  public:
   typedef TMessage message_type;
   typedef TCounter counter_type;
 
   reference_counted_message(const TMessage& msg_,
-                            etl::ireference_counted_message_pool& owner_)
+                            ireference_counted_message_pool& owner_)
       : rc_object(msg_), owner(owner_) {}
 
  private:
-  etl::reference_counted_object<TMessage, TCounter> rc_object;
-  etl::ireference_counted_message_pool& owner;
+  reference_counted_object<TMessage, TCounter> rc_object;
+  ireference_counted_message_pool& owner;
 };
 
 template <typename TMessage>
 using atomic_counted_message =
-    etl::reference_counted_message<TMessage, etl::atomic_int32_t>;
-
-}  // namespace etl
-
-namespace etl {
+    reference_counted_message<TMessage, atomic_int32_t>;
 
 template <typename TCounter>
-class reference_counted_message_pool
-    : public etl::ireference_counted_message_pool {
+class reference_counted_message_pool : public ireference_counted_message_pool {
  public:
   reference_counted_message_pool(
-      etl::imemory_block_allocator& memory_block_allocator_)
+      imemory_block_allocator& memory_block_allocator_)
       : memory_block_allocator(memory_block_allocator_) {}
 
   template <typename TMessage1, typename... TMessages>
   struct pool_message_parameters {
    private:
     static constexpr size_t size1 =
-        sizeof(etl::reference_counted_message<TMessage1, TCounter>);
+        sizeof(reference_counted_message<TMessage1, TCounter>);
 
     static constexpr size_t size2 =
         pool_message_parameters<TMessages...>::max_size;
 
     static constexpr size_t alignment1 = std::alignment_of<
-        etl::reference_counted_message<TMessage1, TCounter>>::value;
+        reference_counted_message<TMessage1, TCounter>>::value;
 
     static constexpr size_t alignment2 =
         pool_message_parameters<TMessages...>::max_alignment;
@@ -167,10 +138,10 @@ class reference_counted_message_pool
   struct pool_message_parameters<TMessage1> {
    public:
     static constexpr size_t max_size =
-        sizeof(etl::reference_counted_message<TMessage1, TCounter>);
+        sizeof(reference_counted_message<TMessage1, TCounter>);
 
     static constexpr size_t max_alignment = std::alignment_of<
-        etl::reference_counted_message<TMessage1, TCounter>>::value;
+        reference_counted_message<TMessage1, TCounter>>::value;
   };
 
  private:
@@ -182,15 +153,12 @@ class reference_counted_message_pool
       const reference_counted_message_pool&) = delete;
 };
 
-using atomic_counted_message_pool =
-    reference_counted_message_pool<etl::atomic_int>;
+using atomic_counted_message_pool = reference_counted_message_pool<atomic_int>;
 
-}  // namespace etl
+constexpr message_id_t MessageId1 = 1U;
+constexpr message_id_t MessageId2 = 2U;
 
-constexpr etl::message_id_t MessageId1 = 1U;
-constexpr etl::message_id_t MessageId2 = 2U;
-
-struct Message1 : public etl::message<MessageId1> {
+struct Message1 : public message<MessageId1> {
   Message1(int i_) : i(i_) {}
 
   Message1(const Message1& oth) { i = oth.i; }
@@ -200,29 +168,25 @@ struct Message1 : public etl::message<MessageId1> {
   int i;
 };
 
-struct Message2 : public etl::message<MessageId2> {
+struct Message2 : public message<MessageId2> {
   ~Message2() override {}
 };
 
 int main() {
   using pool_message_parameters =
-      etl::atomic_counted_message_pool::pool_message_parameters<Message1,
-                                                                Message2>;
+      atomic_counted_message_pool::pool_message_parameters<Message1, Message2>;
 
   using pool_message_parameters =
-      etl::atomic_counted_message_pool::pool_message_parameters<Message1,
-                                                                Message2>;
+      atomic_counted_message_pool::pool_message_parameters<Message1, Message2>;
 
-  etl::fixed_sized_memory_block_allocator<
-      pool_message_parameters::max_size, pool_message_parameters::max_alignment,
-      4U>
+  fixed_sized_memory_block_allocator<pool_message_parameters::max_size,
+                                     pool_message_parameters::max_alignment, 4U>
       memory_allocator;
 
-  etl::atomic_counted_message_pool message_pool(memory_allocator);
+  atomic_counted_message_pool message_pool(memory_allocator);
 
   Message1 message1(6);
-  etl::reference_counted_message<Message1, etl::atomic_int> temp(message1,
-                                                                 message_pool);
-  const etl::ireference_counted_message& rcmessage = temp;
+  reference_counted_message<Message1, atomic_int> temp(message1, message_pool);
+  const ireference_counted_message& rcmessage = temp;
   rcmessage.~ireference_counted_message();
 }
