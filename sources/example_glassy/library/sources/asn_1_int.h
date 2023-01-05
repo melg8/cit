bf1016176ccfceaf342937566ee43dbae657ed7c
Melg Eight <public.melg8@gmail.com>
2023-01-05 08:40:34 +0300

    refactor: use ? instead of if
    Use ? instead of if.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

--- sources/example_glassy/library/sources/asn_1_int.h
+++ sources/example_glassy/library/sources/asn_1_int.h
@@ -70,45 +70,35 @@ std::strong_ordering Compare(
 inline glassy::Result<glassy::Asn1IntOwner>
 Asn1Int::NewUninitialized() noexcept {
   Asn1IntHolder ptr{ASN1_INTEGER_new()};
-  if (!ptr) {
-    return Asn1IntErrc::kAllocationFailure;
-  }
-  return glassy::Result<Asn1IntOwner>{std::move(ptr)};
+  return ptr ? glassy::Result<Asn1IntOwner>{std::move(ptr)}
+             : Asn1IntErrc::kAllocationFailure;
 }
 
 inline glassy::Result<glassy::Asn1IntOwner> glassy::Asn1Int::New(
     Long value) noexcept {
   OUTCOME_TRY(auto result, Asn1Int::NewUninitialized());
-  if (ASN1_INTEGER_set(result.get(), value) == 0) {
-    return Asn1IntErrc::kAllocationFailure;
-  }
-  return {std::move(result)};
+  return ASN1_INTEGER_set(result.get(), value) != 0
+             ? glassy::Result<glassy::Asn1IntOwner>{std::move(result)}
+             : Asn1IntErrc::kAllocationFailure;
 }
 
 inline glassy::Result<glassy::Asn1IntOwner> Asn1Int::New(
     not_null_provider_of<const ASN1_INTEGER*> auto&& view) noexcept {
   Asn1IntHolder ptr{ASN1_INTEGER_dup(GetPtr(view))};
-  if (!ptr) {
-    return Asn1IntErrc::kCopyFailure;
-  }
-  return {std::move(ptr)};
+  return ptr ? glassy::Result<glassy::Asn1IntOwner>{std::move(ptr)}
+             : Asn1IntErrc::kCopyFailure;
 }
 
 inline glassy::Result<glassy::Asn1IntOwner> Asn1Int::Own(
     Asn1IntegerOwnerPtr ptr) noexcept {
-  if (!ptr) {
-    return Asn1IntErrc::kNullPointerFailure;
-  }
-  return glassy::Result<glassy::Asn1IntOwner>{Asn1IntHolder{ptr}};
+  return ptr ? glassy::Result<glassy::Asn1IntOwner>{Asn1IntHolder{ptr}}
+             : Asn1IntErrc::kNullPointerFailure;
 }
 
 inline Result<Long> ToLong(
     not_null_provider_of<const ASN1_INTEGER*> auto&& view) noexcept {
   const auto result = ASN1_INTEGER_get(GetPtr(view));
-  if (result == -1) {
-    return Asn1IntErrc::kConversionFailure;
-  }
-  return result;
+  return result != -1 ? Result<Long>{result} : Asn1IntErrc::kConversionFailure;
 }
 
 }  // namespace glassy
