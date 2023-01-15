2b264e2c02f9139af1153b18003e1e10a9df7c30
Melg Eight <public.melg8@gmail.com>
2023-01-15 11:56:54 +0300

    refactor: introduce testing framework
    Introduce testing framework as separate library.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

---
+++ sources/example_glassy/tests/testing_framework/sources/testing_framework.h
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: © 2023 Melg Eight <public.melg8@gmail.com>
+//
+// SPDX-License-Identifier: MIT
+
+#ifndef TESTING_FRAMEWORK_H
+#define TESTING_FRAMEWORK_H
+
+#include <doctest/doctest.h>
+
+#endif  // TESTING_FRAMEWORK_H
