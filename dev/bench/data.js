0dae0fa2157e60b5d0cb7f8e12c638045ca6496e
github-action-benchmark <github@users.noreply.github.com>
2023-01-17 07:06:50 +0000

    add Catch2 Benchmark g++ (catch2) benchmark result for e6f491a92b48f93522ad3ab7ea8102c6b5670adc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1673939154197,
+  "lastUpdate": 1673939210707,
   "repoUrl": "https://github.com/melg8/cit",
   "entries": {
     "Catch2 Benchmark clang++": [
@@ -45,6 +45,53 @@ window.BENCHMARK_DATA = {
             "range": "+/- 293.487",
             "unit": "us",
             "extra": "100 samples\n15 iterations"
+          }
+        ]
+      }
+    ],
+    "Catch2 Benchmark g++": [
+      {
+        "commit": {
+          "author": {
+            "email": "public.melg8@gmail.com",
+            "name": "Melg Eight",
+            "username": "melg8"
+          },
+          "committer": {
+            "email": "public.melg8@gmail.com",
+            "name": "Melg Eight",
+            "username": "melg8"
+          },
+          "distinct": true,
+          "id": "e6f491a92b48f93522ad3ab7ea8102c6b5670adc",
+          "message": "test: move usability tests to benchmark\n\nMove usability tests to benchmark.\n\nSigned-off-by: Melg Eight <public.melg8@gmail.com>",
+          "timestamp": "2023-01-17T09:56:06+03:00",
+          "tree_id": "9eeae98b08fe080bd0998ce1a51cf2d286720f8e",
+          "url": "https://github.com/melg8/cit/commit/e6f491a92b48f93522ad3ab7ea8102c6b5670adc"
+        },
+        "date": 1673939210214,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "compare raw pointers",
+            "value": 2.25284,
+            "range": "+/- 191.422",
+            "unit": "us",
+            "extra": "100 samples\n26 iterations"
+          },
+          {
+            "name": "compare smart pointers",
+            "value": 2.43567,
+            "range": "+/- 226.373",
+            "unit": "us",
+            "extra": "100 samples\n25 iterations"
+          },
+          {
+            "name": "compare modernized",
+            "value": 3.66461,
+            "range": "+/- 321.324",
+            "unit": "us",
+            "extra": "100 samples\n16 iterations"
           }
         ]
       }
