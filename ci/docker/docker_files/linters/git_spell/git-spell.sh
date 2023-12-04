28cc0e6873a1e41a1e7dcf99f63eeb929a442151
melg8 <public.melg8@gmail.com>
2023-12-04 05:22:21 +0300

    fix: add git log spelling check
    Add git log spelling check using cspell.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>
    Signed-off-by: melg8 <public.melg8@gmail.com>

---
+++ ci/docker/docker_files/linters/git_spell/git-spell.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: © 2023 Melg Eight <public.melg8@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+git log --pretty="%B" |
+	cspell -v --config=./ci/checks/dictionaries/cspell.json stdin://git_log_temp.md
+echo "git-spell OK"
