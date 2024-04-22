d2802156dcdecf3758087be2e31460ab2d6af465
melg8 <public.melg8@gmail.com>
2024-04-22 14:53:22 +0300

    chore: add adventure land application
    Add advenutre land application.
    
    Signed-off-by: Melg Eight <public.melg8@gmail.com>

---
+++ sources/adventure_land/application/sources/main.cpp
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: © 2024 Melg Eight <public.melg8@gmail.com>
+//
+// SPDX-License-Identifier: MIT
+
+#include <iostream>
+
+int main() { std::cout << "Hello Adventure" << std::endl; }
